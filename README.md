9313a5d7ce07e86398546f9da9fc8396b1dcaf8a
timjzee <timjzee@gmail.com>
2023-04-18 11:50:08 +0200

    Bugfix & refactor

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # diglinplus
 Scripts for DigLin+ data processing and analysis
+
+## Instructions
+- start local mongodb server `brew services start mongodb-community@6.0`
--- config.py
+++ config.py
@@ -2,6 +2,6 @@ from os import environ
 from dotenv import load_dotenv
 
 load_dotenv()
-CONNECT_STR = environ("CONNECT_STR")
+CONNECT_STR = environ["CONNECT_STR"]
 
 col_name = "placeholder"
